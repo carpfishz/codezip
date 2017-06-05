6e6f3327ea3b474d764631a806919a45e72458ac
carpfishz <lsml3541@gmail.com>
2017-06-06 03:47:48 +0900

    implement choice language function

--- nologmain.php
+++ nologmain.php
@@ -9,18 +9,13 @@
 session_start();
 require_once 'class.user.php';
 $login = new user();
+
 if($login->is_loggedin()!="") {
     $login->redirection('main.php');
 }
 
-
-if(!isset($_POST['val1'])){
-    $codejs = './default.js';
-}else{
-    $codejs = $_POST['val1'];
-    $slt = $_POST['val2'];
-}
-
+$lang = $_GET['lang'];
+$stmt_temp = [];
 
 ?>
 
@@ -35,61 +30,32 @@ if(!isset($_POST['val1'])){
         <div id="form-wrapper">
             <form action="run.php" method="POST" id="code_form">
                 <textarea id="code_area" class="codemirror-textarea" name="user_code" autocorrect="off" autocomplete="off" autocapitalize="off" spellcheck="false" wrap="off" style="width: 100%; height:600px;">print("hello!")</textarea>
-                <select id="select_code" name="select_code" onchange="setValue()">
-                    <option id="default" value="python">Python</option>
-                    <option value="java">Java</option>
-                    <option value="c">C</option>
-                    <option value="ruby">Ruby</option>
-                </select>
+                <input type="radio" name="lang" value="C" onclick="location.href='nologmain.php?lang=C'" id="langC">C
+                <input type="radio" name="lang" value="C++" onclick="location.href='nologmain.php?lang=C%2B%2B'" id="langCplus">C++
+                <input type="radio" name="lang" value="Java" onclick="location.href='nologmain.php?lang=Java'" id="langJava">Java
+                <input type="radio" name="lang" value="Python" onclick="location.href='nologmain.php?lang=Python'" id="langPython">Python
+                <input type="radio" name="lang" value="Python3" onclick="location.href='nologmain.php?lang=Python3'" id="langPython3" checked="checked">Python3
+                <input type="radio" name="lang" value="Ruby" onclick="location.href='nologmain.php?lang=Ruby'" id="langRuby">Ruby
                 <input type="submit" value="Run" name="submit" id="code_submit">
             </form>
-
-            <script>
-                var e = document.getElementById("select_code");
-                var strUser = e.options[e.selectedIndex].value;
-                console.log(strUser);
-
-                function setValue() {
-                    var e = document.getElementById("select_code");
-                    var strUser = e.options[e.selectedIndex].value;
-                    if(strUser == 'c') {
-                        document.getElementById("code_mode").src = 'mode/clike/clike.js';
-//                        document.getElementById("code_js").src = './mode_c.js';
-                    } else if (strUser == 'java') {
-                        document.getElementById("code_mode").src = 'mode/clike/clike.js';
-//                        document.getElementById("code_js").src = './mode_java.js';
-                    } else if (strUser == 'python') {
-                        document.getElementById("code_mode").src = 'mode/python/python.js';
-                        document.getElementById("code_js").src = './default.js';
-                    }
-                    console.log(strUser);
-                }
-            </script>
 
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
             <script src="lib/codemirror.js"></script>
             <link rel="stylesheet" href="lib/codemirror.css">
             <script id="code_mode" src="mode/python/python.js"></script>
-            <script id="code_js" src="<?=$codejs;?>"></script>
+            <script id="code_js" src="default.js"></script>
             <script src="custom.js"></script>
-
             <script src="mode/clike/clike.js"></script>
 <!--            <script src="./mode_c.js"></script>-->
 <!--            <script src="./mode_java.js"></script>-->
+            <script> <?php if($lang == 'C') {echo "$('#langC').attr('checked', 'checked');";}?>
+                <?php if($lang == 'C++') {echo "$('#langCplus').attr('checked', 'checked');";}?>
+                <?php if($lang == 'Java') {echo "$('#langJava').attr('checked', 'checked');";}?>
+                <?php if($lang == 'Python') {echo "$('#langPython').attr('checked', 'checked');";}?>
+                <?php if($lang == 'Python3') {echo "$('#langPython3').attr('checked', 'checked');";}?>
+                <?php if($lang == 'Ruby') {echo "$('#langRuby').attr('checked', 'checked');";}?>
+            </script>
         </div>
     </div>
 </body>
 </html>
-
-
-<?php
-    if(isset($_POST['val1'])){
-    ?>
-    <script>
-        var slt = '<?=$slt;?>';
-        $('#default').text(slt);
-
-    </script>
-<?php
-}
-?>
--- run.php
+++ run.php
@@ -12,19 +12,79 @@ function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzAB
 $rand_str = random_str(8);
 
 if(isset($_POST['submit'])) {
-    $myfile = fopen("dockertest/". $rand_str . ".py", "w");
+    $lang = $_POST['lang'];
     $txt = $_POST['user_code'];
-    fwrite($myfile, $txt);
-    fclose($myfile);
 
-    //$command = "sudo /usr/bin/docker run --rm -m=30m --stop-signal=SIGTERM --stop-timeout=10 -v /var/www/html/dockertest/" . $rand_str . ".py" . ":/" . $rand_str . ".py" . " festival/demo1 python " . $rand_str . ".py" . " > " . $rand_str . ".txt 2>&1";
+    if($lang == 'C') {
+        $myfile = fopen("dockertest/". $rand_str . ".c", "w");
+        fwrite($myfile, $txt);
+        fclose($myfile);
+        $command = "sudo /usr/bin/docker run --rm -m=30m --stop-signal=SIGTERM --stop-timeout=10 -v /var/www/html/dockertest/" . $rand_str . ".c" . ":/" . $rand_str . ".c" . " gcc:7.1 /bin/bash -c 'gcc " . $rand_str . ".c -o " . $rand_str . " ; ./". $rand_str . " 2>&1'";
+        $output = shell_exec($command);
+        $output = str_replace("\n", "<br>", $output);
+        echo "<div style='word-break:break-all; word-wrap:break-word;width=100%'>".$output."</div>";
+        unlink("dockertest/" . $rand_str . ".c");
 
-    $command = "sudo /usr/bin/docker run --rm -m=30m --stop-signal=SIGTERM --stop-timeout=10 -v /var/www/html/dockertest/" . $rand_str . ".py" . ":/" . $rand_str . ".py" . " festival/demo1 python " . $rand_str . ".py" . " 2>&1";
-    $output = shell_exec($command);
-    $output = str_replace("\n", "<br>", $output);
-    echo "<div style='word-break:break-all; word-wrap:break-word;width=100%'>".$output."</div>";
+    } elseif ($lang == 'C++') {
+        $myfile = fopen("dockertest/". $rand_str . ".cpp", "w");
+        fwrite($myfile, $txt);
+        fclose($myfile);
+//        $command = "sudo /usr/bin/docker run --rm -m=30m --stop-signal=SIGTERM --stop-timeout=10 -v /var/www/html/dockertest/" . $rand_str . ".c" . ":/" . $rand_str . ".c" . " gcc:7.1 /bin/bash -c 'gcc " . $rand_str . ".c -o " . $rand_str . " ; ./". $rand_str . " 2>&1'";
+//        echo $command;
+        $command = "g++ dockertest/" . $rand_str . ".cpp -o dockertest/" . $rand_str . "; ./dockertest/" . $rand_str . " 2>&1";
+        $output = shell_exec($command);
+        $output = str_replace("\n", "<br>", $output);
+        echo "<div style='word-break:break-all; word-wrap:break-word;width=100%'>".$output."</div>";
+        unlink("dockertest/" . $rand_str . ".cpp");
+        unlink("dockertest/" . $rand_str);
 
-    unlink("dockertest/" . $rand_str . ".py");
+    } elseif ($lang == 'Java') {
+        $myfile = fopen("dockertest/". $rand_str . ".java", "w");
+        fwrite($myfile, $txt);
+        fclose($myfile);
+        $command = "sudo /usr/bin/docker run --rm -m=30m --stop-signal=SIGTERM --stop-timeout=10 -v /var/www/html/dockertest/" . $rand_str . ".java" . ":/" . $rand_str . ".java" . " java:openjdk-8-jdk /bin/bash -c 'javac " . $rand_str . ".java ; java codezip" . " 2>&1'";
+        $output = shell_exec($command);
+        $output = str_replace("\n", "<br>", $output);
+        echo "<div style='word-break:break-all; word-wrap:break-word;width=100%'>".$output."</div>";
+        unlink("dockertest/" . $rand_str . ".java");
+
+    } elseif ($lang == 'Python') {
+        $myfile = fopen("dockertest/". $rand_str . ".py", "w");
+        fwrite($myfile, $txt);
+        fclose($myfile);
+        $command = "sudo /usr/bin/docker run --rm -m=30m --stop-signal=SIGTERM --stop-timeout=10 -v /var/www/html/dockertest/" . $rand_str . ".py" . ":/" . $rand_str . ".py" . " python:2.7.13-slim python " . $rand_str . ".py" . " 2>&1";
+        $output = shell_exec($command);
+        $output = str_replace("\n", "<br>", $output);
+        echo "<div style='word-break:break-all; word-wrap:break-word;width=100%'>".$output."</div>";
+        unlink("dockertest/" . $rand_str . ".py");
+
+    } elseif ($lang == 'Python3') {
+        $myfile = fopen("dockertest/". $rand_str . ".py", "w");
+        fwrite($myfile, $txt);
+        fclose($myfile);
+
+        //$command = "sudo /usr/bin/docker run --rm -m=30m --stop-signal=SIGTERM --stop-timeout=10 -v /var/www/html/dockertest/" . $rand_str . ".py" . ":/" . $rand_str . ".py" . " festival/demo1 python " . $rand_str . ".py" . " > " . $rand_str . ".txt 2>&1";
+
+        $command = "sudo /usr/bin/docker run --rm -m=30m --stop-signal=SIGTERM --stop-timeout=10 -v /var/www/html/dockertest/" . $rand_str . ".py" . ":/" . $rand_str . ".py" . " festival/demo1 python " . $rand_str . ".py" . " 2>&1";
+        $output = shell_exec($command);
+        $output = str_replace("\n", "<br>", $output);
+        echo "<div style='word-break:break-all; word-wrap:break-word;width=100%'>".$output."</div>";
+        unlink("dockertest/" . $rand_str . ".py");
+
+    } elseif ($lang == 'Ruby') {
+        $myfile = fopen("dockertest/". $rand_str . ".rb", "w");
+        fwrite($myfile, $txt);
+        fclose($myfile);
+        $command = "sudo /usr/bin/docker run --rm -m=30m --stop-signal=SIGTERM --stop-timeout=10 -v /var/www/html/dockertest/" . $rand_str . ".rb" . ":/" . $rand_str . ".rb" . " ruby:2.1.10-slim ruby " . $rand_str . ".rb" . " 2>&1";
+        $output = shell_exec($command);
+        $output = str_replace("\n", "<br>", $output);
+        echo "<div style='word-break:break-all; word-wrap:break-word;width=100%'>".$output."</div>";
+        unlink("dockertest/" . $rand_str . ".rb");
+
+    } else {
+        header("http://223.194.105.180/nologmain.php");
+    }
+
 } else {
     // echo "fail";
     header("http://223.194.105.180/nologmain.php");
